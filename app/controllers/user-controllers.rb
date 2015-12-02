63ad84db60e50ebc711b181095394b92d57eb2ba
Sam <samkahchiin@gmail.com>
2015-12-02 16:56:00 +0800

    done the user part

---
+++ app/controllers/user-controllers.rb
@@ -0,0 +1,38 @@
+
+# Create new user
+post '/users' do
+	user = User.create(name: params[:name], email: params[:email], password: params[:password])
+	redirect "/users/#{user.id}"
+end
+
+# user login
+post '/users/login' do
+	user = User.find_by(email: params[:email], password: params[:password]) 
+	session[:user_id] = user.id
+	redirect "/users/#{user.id}"
+end
+
+# user profile page
+get '/users/:id' do
+	@user = User.find(params[:id])
+	erb :"user/home"
+end
+
+# Edit user
+get '/users/:id/edit' do
+	@user = User.find(params[:id])
+	erb :"user/edit"
+end
+
+# Update user
+patch "/users/:id" do
+	user = User.find(params[:id])
+	user.update(name: params[:name], email: params[:email], password: params[:password], description: params[:description])
+	redirect "/users/#{user.id}"
+end
+
+delete "/users/:id" do
+	user = User.find(params[:id])
+	user.destroy
+	redirect "/"	
+end
