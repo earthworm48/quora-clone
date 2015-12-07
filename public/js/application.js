6034998cfc654584096673d8421206f6fa0453b1
Sam <samkahchiin@gmail.com>
2015-12-07 16:47:27 +0800

    done the read more function by refer from stackflow

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
+
+	var minimized_elements = $('.truncated_answer_content');
+    var minimize_character_count = 100;    
+
+    minimized_elements.each(function(){    
+        var t = $(this).text();        
+        if(t.length < minimize_character_count ) return;
+
+        $(this).html(
+            t.slice(0,minimize_character_count )+'<span>... </span><a href="#" class="more">(More)</a>'+
+            '<span style="display:none;">'+ t.slice(minimize_character_count ,t.length)+' <a href="#" class="less">Less</a></span>'
+        );
+
+    }); 
+
+    $('a.more', minimized_elements).click(function(event){
+        event.preventDefault();
+        $(this).hide().prev().hide();
+        $(this).next().show();        
+    });
+
+    $('a.less', minimized_elements).click(function(event){
+        event.preventDefault();
+        $(this).parent().hide().prev().show().prev().show();    
+    });
+
 	$(".upvote-button").click(function(){
 		var url = $(this).data('vote-url');
 
